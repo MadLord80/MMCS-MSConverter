1ea76de50fc5ca740a1d866bcb0667f33396c13b
MadLord <madlord@list.ru>
2017-04-18 17:30:33 +0700

    prepare to async

--- MultiTraConv/Form1.Designer.cs
+++ MultiTraConv/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MultiTraConv
 {
-    partial class Form1
+    partial class MultiTraConv
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,12 +28,211 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.mp3dir_dialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.mp3dir_button = new System.Windows.Forms.Button();
+            this.mp3_path = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.oma_path = new System.Windows.Forms.TextBox();
+            this.omadir_button = new System.Windows.Forms.Button();
+            this.omadir_dialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.FilesTable = new System.Windows.Forms.ListView();
+            this.filename = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.filestatus = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.all_count = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.conv_count = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mp3dir_dialog
+            // 
+            this.mp3dir_dialog.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            this.mp3dir_dialog.ShowNewFolderButton = false;
+            // 
+            // mp3dir_button
+            // 
+            this.mp3dir_button.Location = new System.Drawing.Point(926, 14);
+            this.mp3dir_button.Name = "mp3dir_button";
+            this.mp3dir_button.Size = new System.Drawing.Size(24, 23);
+            this.mp3dir_button.TabIndex = 0;
+            this.mp3dir_button.Text = "...";
+            this.mp3dir_button.UseVisualStyleBackColor = true;
+            this.mp3dir_button.Click += new System.EventHandler(this.mp3dir_button_Click);
+            // 
+            // mp3_path
+            // 
+            this.mp3_path.Location = new System.Drawing.Point(141, 17);
+            this.mp3_path.Name = "mp3_path";
+            this.mp3_path.ReadOnly = true;
+            this.mp3_path.Size = new System.Drawing.Size(779, 20);
+            this.mp3_path.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "MP3 directory (input)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "OMA directory (output)";
+            // 
+            // oma_path
+            // 
+            this.oma_path.Location = new System.Drawing.Point(141, 50);
+            this.oma_path.Name = "oma_path";
+            this.oma_path.ReadOnly = true;
+            this.oma_path.Size = new System.Drawing.Size(779, 20);
+            this.oma_path.TabIndex = 4;
+            // 
+            // omadir_button
+            // 
+            this.omadir_button.Location = new System.Drawing.Point(926, 48);
+            this.omadir_button.Name = "omadir_button";
+            this.omadir_button.Size = new System.Drawing.Size(24, 23);
+            this.omadir_button.TabIndex = 5;
+            this.omadir_button.Text = "...";
+            this.omadir_button.UseVisualStyleBackColor = true;
+            this.omadir_button.Click += new System.EventHandler(this.omadir_button_Click);
+            // 
+            // omadir_dialog
+            // 
+            this.omadir_dialog.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // FilesTable
+            // 
+            this.FilesTable.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.filename,
+            this.filestatus});
+            this.FilesTable.GridLines = true;
+            this.FilesTable.Location = new System.Drawing.Point(15, 134);
+            this.FilesTable.Name = "FilesTable";
+            this.FilesTable.Size = new System.Drawing.Size(935, 363);
+            this.FilesTable.TabIndex = 6;
+            this.FilesTable.UseCompatibleStateImageBehavior = false;
+            this.FilesTable.View = System.Windows.Forms.View.Details;
+            // 
+            // filename
+            // 
+            this.filename.Text = "File";
+            this.filename.Width = 746;
+            // 
+            // filestatus
+            // 
+            this.filestatus.Text = "Status";
+            this.filestatus.Width = 185;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Process summary: ";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(113, 115);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(837, 13);
+            this.progressBar.TabIndex = 8;
+            // 
+            // all_count
+            // 
+            this.all_count.AutoSize = true;
+            this.all_count.Location = new System.Drawing.Point(451, 96);
+            this.all_count.Name = "all_count";
+            this.all_count.Size = new System.Drawing.Size(13, 13);
+            this.all_count.TabIndex = 9;
+            this.all_count.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(483, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(12, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "/";
+            // 
+            // conv_count
+            // 
+            this.conv_count.AutoSize = true;
+            this.conv_count.Location = new System.Drawing.Point(492, 96);
+            this.conv_count.Name = "conv_count";
+            this.conv_count.Size = new System.Drawing.Size(13, 13);
+            this.conv_count.TabIndex = 11;
+            this.conv_count.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(833, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 32);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MultiTraConv
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(970, 509);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.conv_count);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.all_count);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.FilesTable);
+            this.Controls.Add(this.omadir_button);
+            this.Controls.Add(this.oma_path);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mp3_path);
+            this.Controls.Add(this.mp3dir_button);
+            this.MinimumSize = new System.Drawing.Size(986, 547);
+            this.Name = "MultiTraConv";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MultiTraConv";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog mp3dir_dialog;
+        private System.Windows.Forms.Button mp3dir_button;
+        private System.Windows.Forms.TextBox mp3_path;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox oma_path;
+        private System.Windows.Forms.Button omadir_button;
+        private System.Windows.Forms.FolderBrowserDialog omadir_dialog;
+        private System.Windows.Forms.ListView FilesTable;
+        private System.Windows.Forms.ColumnHeader filename;
+        private System.Windows.Forms.ColumnHeader filestatus;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label all_count;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label conv_count;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- MultiTraConv/Form1.cs
+++ MultiTraConv/Form1.cs
@@ -1,20 +1,139 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
 
 namespace MultiTraConv
 {
-    public partial class Form1 : Form
+    public partial class MultiTraConv : Form
     {
-        public Form1()
+        private Int32 _start_form_height = 547;
+        private Int32 _start_table_height = 363;
+        //private FileInfo[] mp3_files;
+
+        public MultiTraConv()
         {
             InitializeComponent();
+            this.MaximumSize = new Size(986,800);
+            this.SizeChanged += new EventHandler(this.MultiTraConv_SizeChanged);
+        }
+
+        private void MultiTraConv_SizeChanged(Object sender, EventArgs e)
+        {
+            var new_table_height = (this.Height > _start_form_height)
+                ? this.Height - _start_form_height
+                : _start_form_height - this.Height;
+            this.FilesTable.Size = new Size(this.FilesTable.Width, 
+                (this.Height > _start_form_height) 
+                ? _start_table_height + new_table_height 
+                : _start_table_height - new_table_height
+            );
+        }
+
+        private void mp3dir_button_Click(object sender, EventArgs e)
+        {
+            if (this.mp3dir_dialog.ShowDialog() == DialogResult.OK)
+            {
+string temp_dir = "D:\\temp\\Мои документы\\Музыка с дисков";
+this.mp3dir_dialog.SelectedPath = temp_dir;
+
+                var mp3_dir = new DirectoryInfo(this.mp3dir_dialog.SelectedPath);
+                FileInfo[] mp3_files = mp3_dir.GetFiles("*.mp3", SearchOption.AllDirectories);
+                if (mp3_files.Length > 0)
+                {
+                    this.all_count.Text = mp3_files.Length.ToString();
+                    //this.conv_count.Text = mp3_files.Length.ToString();
+                    this.mp3_path.Text = this.mp3dir_dialog.SelectedPath;
+                    fill_files_table(mp3_files);
+                }
+                else
+                {
+                    MessageBox.Show("No MP3 files in directory!");
+                }
+            }
+        }
+
+        private void omadir_button_Click(object sender, EventArgs e)
+        {
+            if (this.omadir_dialog.ShowDialog() == DialogResult.OK)
+            {
+string temp_dir = "D:\\tmp";
+this.omadir_dialog.SelectedPath = temp_dir;
+
+                var oma_dir = new DirectoryInfo(this.omadir_dialog.SelectedPath);
+                if (oma_dir.GetFiles("*").Length > 0)
+                {
+                    MessageBox.Show("OMA directory is not empty!");
+                }
+                this.oma_path.Text = this.omadir_dialog.SelectedPath;
+            }
+        }
+
+        private void fill_files_table(FileInfo[] mp3_files)
+        {
+            this.FilesTable.Items.Clear();
+            foreach (FileInfo file in mp3_files)
+            {
+                ListViewItem row = new ListViewItem(file.FullName);
+                row.SubItems.Add("ready");
+                this.FilesTable.Items.Add(row);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.FilesTable.Items.Count < 1) return;
+            var app_dir = Path.GetDirectoryName(Application.ExecutablePath);
+
+            Process traconv = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo(app_dir + "\\TraConv.exe");
+            startInfo.CreateNoWindow = true;
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+
+            int conv_cnt = 0;
+            foreach (ListViewItem row in this.FilesTable.Items)
+            {
+                string filename = row.Text;
+                string add_path = filename.Remove(0, mp3_path.TextLength);
+                add_path = add_path.Remove(add_path.Length - 3, 3) + "oma";
+                string new_path = oma_path.Text + add_path;
+                if (File.Exists(new_path))
+                {
+                    row.SubItems[1].Text = "skipped (file exists)";
+                    this.FilesTable.Refresh();
+                    conv_cnt++;
+                    this.conv_count.Text = conv_cnt.ToString();
+                    this.conv_count.Refresh();
+                    continue;
+                }
+                string new_path_quoted = '"' + new_path + '"';
+                row.SubItems[1].Text = "converting...";
+                this.FilesTable.Refresh();
+                startInfo.Arguments = '"' + filename + '"' + " --Convert --FileType OMA --BitRate 352000 --Output " + new_path_quoted;
+
+                traconv.StartInfo = startInfo;
+                traconv.Start();
+                string t_out = traconv.StandardOutput.ReadToEnd();
+                traconv.WaitForExit();
+                if (! t_out.Contains("Location=" + new_path))
+                {
+                    row.SubItems[1].Text = "Error: " + startInfo.Arguments;
+                    this.FilesTable.Refresh();
+                    continue;
+                }
+                row.SubItems[1].Text = "convered";
+                this.FilesTable.Refresh();
+                conv_cnt++;
+                this.conv_count.Text = conv_cnt.ToString();
+                this.conv_count.Refresh();
+            }            
+        }
+
+        private async void get_result (Process proc, ListViewItem item)
+        {
+            string p_out = await proc.StandardOutput.ReadToEndAsync();
         }
     }
 }
